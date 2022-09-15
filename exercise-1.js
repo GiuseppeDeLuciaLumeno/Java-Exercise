32c427c04012655c6dd0dfd5f28916db8a88c9fc
Giuseppe De Lucia Lumeno <giupetrucci@libero.it>
2022-09-15 12:24:51 +0200

    fix

--- exercise-1.js
+++ exercise-1.js
@@ -1,63 +1,13 @@
-// Solution 1:
 
 let personName = "Paul ";
 
-
-function canPlay()
-{
-  if (true)
-  {
+function canPlay() {
+  if (true) {
     personName += "plays football";
-  }
 
-  /* personName += ' plays football'; */ // Argh! personName is not defined
-
-  console.log(personName);
+  } console.log(personName);
 }
 
 canPlay();
-//---------------------------------------------------------------------
 
 
-
-//Solution 2:
-
-
-let playerName = "Paul";
-
-
-function getPLayerName()
-{
-  if (playerName)
-  {
-    playerName += " plays football";
-  } else
-  {
-    playerName += "He doesn't";
-  }
-}
-
-getPLayerName()
-console.log(playerName);
-//------------------------------------------------------
-
-
-//Solution 3:
-
-let action = "plays football";
-
-function whoIsPlaying(name) {
-
-  if (name === String("Paul"))
-  {
-    name += action;
-  } else {
-    name += " doesn't";
-  }
-
-}
-
-whoIsPlaying("Paul");
-console.log(playerName);
-
-//----------------------------------------------------
