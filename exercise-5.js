f0c750bfeedd9b49a0672861c8ffb8d644ce051f
Giuseppe De Lucia Lumeno <giupetrucci@libero.it>
2022-10-14 01:24:43 +0200

    ex

---
+++ exercise-5.js
@@ -0,0 +1,33 @@
+function calculateAverageAge(persons) {
+let result = 0
+let finalCalcule;
+
+for (let i = 0; i < persons.length; i++) {
+  const element = persons[i];
+  result += element.age;
+  finalCalcule = Math.round(result / persons.length);
+}
+return `L'età media del gruppo è di: ${finalCalcule} anni!`
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 },
+];
+
+const averageAge = calculateAverageAge(persons);
+console.log(persons);
+console.log(averageAge);
+
+
+//ho aggiunto Math.round() per arrotondare all'intero più vicino
+
+
