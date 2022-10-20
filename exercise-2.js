756fdf51d20e91417b7245b305f442f4ac5276c9
Giuseppe De Lucia Lumeno <giupetrucci@libero.it>
2022-10-20 16:20:15 +0200

    ex

---
+++ exercise-2.js
@@ -0,0 +1,12 @@
+const person1 = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 25
+};
+
+
+let person2 = {...person1};
+person2.firstName = "Simon";
+
+console.log(person1);
+console.log(person2);
