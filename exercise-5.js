233a97f36b901db5bc852aa9bc8ed92ce41f18ad
Giuseppe De Lucia Lumeno <giupetrucci@libero.it>
2022-10-24 00:27:22 +0200

    ex

---
+++ exercise-5.js
@@ -0,0 +1,56 @@
+class BankAccount {
+  #amount = 0;
+
+  constructor(initialAmount) {
+    this.#amount = initialAmount;
+  }
+
+  deposit(amount) {
+    this.#amount += amount;
+  }
+
+  withdraw(amount) {
+    this.#amount -= amount;
+  }
+
+  view() {
+   return console.log(this.#amount) ;
+  }
+}
+
+
+class BankAccountVip extends BankAccount{
+  constructor(initialAmount){
+    super(initialAmount)
+  }
+
+  deposit(amount){
+     super.deposit(amount);
+
+    if (amount >= 1000) {
+      return this.deposit((amount * 3)/100);
+    }
+  }
+}
+
+
+const bankAccountVip = new BankAccountVip(1000);
+bankAccountVip.deposit(500);
+bankAccountVip.deposit(1200);
+bankAccountVip.withdraw(800);
+bankAccountVip.deposit(3500);
+bankAccountVip.view();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
